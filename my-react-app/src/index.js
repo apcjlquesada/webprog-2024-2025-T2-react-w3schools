87b7f194658a9932d1c784231eabb40444cf8735
Joe Gene Quesada <52223592+apcjlquesada@users.noreply.github.com>
2024-11-30 17:22:48 +0800

    Pass Data

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Car from './Car.js';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Car />);
+function Car(props) {
+    return <h2>I am a { props.brand.model }!</h2>;
+  }
+  
+  function Garage() {
+    const carInfo = { name: "Ford", model: "Mustang" };
+    return (
+      <>
+          <h1>Who lives in my garage?</h1>
+          <Car brand={ carInfo } />
+      </>
+    );
+  }
+  
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(<Garage />);
+  
+                
