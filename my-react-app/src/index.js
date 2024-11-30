98be04f52d26b9d21752409e57ca698a77ed0a79
Joe Gene Quesada <52223592+apcjlquesada@users.noreply.github.com>
2024-11-30 17:17:05 +0800

    Components in Components

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -2,10 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 function Car() {
-  return <h2>Hi, I am a Car!</h2>;
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Car />);
-
+    return <h2>I am a Car!</h2>;
+  }
+  
+  function Garage() {
+    return (
+      <>
+          <h1>Who lives in my Garage?</h1>
+          <Car />
+      </>
+    );
+  }
+  
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(<Garage />);
+  
               
