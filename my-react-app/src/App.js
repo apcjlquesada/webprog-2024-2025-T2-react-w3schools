60070026cba8193c1fca176c76ebfd752e66bd4a
Joe Gene Quesada <52223592+apcjlquesada@users.noreply.github.com>
2024-11-30 16:19:10 +0800

    update

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -4,22 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <h1>Hello World!</h1>
-        <table>
-    <tr>
-      <th>Name</th>
-    </tr>
-    <tr>
-      <td>John</td>
-    </tr>
-    <tr>
-      <td>Elsa</td>
-    </tr>
-  </table>
-        
-    </div>
-  );
-}
+  <ul>
+    <li>Apples</li>
+    <li>Bananas</li>
+    <li>Cherries</li>
+  </ul>
+);
 
 
 
