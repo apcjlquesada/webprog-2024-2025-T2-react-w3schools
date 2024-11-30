35993062b530a598726a4d0b43a59a9993135dd7
Joe Gene Quesada <52223592+apcjlquesada@users.noreply.github.com>
2024-11-30 17:25:45 +0800

    Keys

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -2,20 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 function Car(props) {
-    return <h2>I am a { props.brand.model }!</h2>;
+    return <li>I am a { props.brand }</li>;
   }
   
   function Garage() {
-    const carInfo = { name: "Ford", model: "Mustang" };
+    const cars = [
+      {id: 1, brand: 'Ford'},
+      {id: 2, brand: 'BMW'},
+      {id: 3, brand: 'Audi'}
+    ];
     return (
       <>
           <h1>Who lives in my garage?</h1>
-          <Car brand={ carInfo } />
+          <ul>
+          {cars.map((car) => <Car key={car.id} brand={car.brand} />)}
+        </ul>
       </>
     );
   }
   
   const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(<Garage />);
-  
-                
