1694e112f2fb5658200eb42bfe00b7d2fd145abb
Joe Gene Quesada <52223592+apcjlquesada@users.noreply.github.com>
2024-11-30 17:14:53 +0800

    Rendering a Component

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const myElement = (
-  <ul>
-    <li>Apples</li>
-    <li>Bananas</li>
-    <li>Cherries</li>
-  </ul>
-);
+function Car() {
+  return <h2>Hi, I am a Car!</h2>;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myElement);
+root.render(<Car />);
 
               
