77261f58af4c13c118a203cc5e78b3524d7cf02a
Joe Gene Quesada <52223592+apcjlquesada@users.noreply.github.com>
2024-11-30 17:19:51 +0800

    Components in Files

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,20 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Car from './Car.js';
 
-function Car() {
-    return <h2>I am a Car!</h2>;
-  }
-  
-  function Garage() {
-    return (
-      <>
-          <h1>Who lives in my Garage?</h1>
-          <Car />
-      </>
-    );
-  }
-  
-  const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(<Garage />);
-  
-              
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Car />);
