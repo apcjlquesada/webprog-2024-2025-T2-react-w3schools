aaf77ff29659b6744b8f5966113c76389ee3b4c3
Joe Gene Quesada <52223592+apcjlquesada@users.noreply.github.com>
2024-12-02 12:18:31 +0800

    update

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,20 +1,3 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './App.css';
-
-const Header = () => {
-  return (
-    <>
-      <h1>Hello Style!</h1>
-      <p>Add a little style!.</p>
-    </>
-  );
-}
-
-
-
-
 import React from 'react';
 import DataFetcher from './DataFetcher.js';
 
